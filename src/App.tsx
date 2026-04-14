5b0a63168922bf76a12be653859f3c2f47a22722
gyandra <gyandranaupal14@gmail.com>
2026-04-14 22:10:12 +0700

    feat: call file navbar and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import Router from "./router/router";
+import Navbar from "./layout/navbar";
+import Footer from "./layout/footer";
 export default function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Router />
+      <Footer />
     </BrowserRouter>
   );
 }
