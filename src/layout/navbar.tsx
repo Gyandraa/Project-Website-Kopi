e563134fb5d6806a974c675f336080033fd7326a
gyandra <gyandranaupal14@gmail.com>
2026-04-14 22:09:13 +0700

    feat: add styling for navbar

---
+++ src/layout/navbar.tsx
@@ -0,0 +1,24 @@
+import { PiShoppingCartSimpleFill } from "react-icons/pi";
+
+export default function Navbar() {
+  return (
+    <div className="flex items-center justify-between py-4 px-8 bg-white shadow-md">
+      <div className="flex-1">
+        <h2 className="font-sans text-3xl font-extrabold tracking-tighter text-gray-900">
+          KopiKita
+        </h2>
+      </div>
+
+      <div className="flex-1 flex justify-center gap-6 text-gray-800">
+        <h3 className="cursor-pointer hover:text-black">Home</h3>
+        <h3 className="cursor-pointer hover:text-black">Location</h3>
+        <h3 className="cursor-pointer hover:text-black">Menu</h3>
+        <h3 className="cursor-pointer hover:text-black">Order</h3>
+      </div>
+
+      <div className="flex-1 flex justify-end">
+        <PiShoppingCartSimpleFill size={24} className="cursor-pointer " />
+      </div>
+    </div>
+  );
+}
