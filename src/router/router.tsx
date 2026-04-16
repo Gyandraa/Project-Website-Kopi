dc0ed2aad648a8cffc31d99e51123a0316a59df5
gyandra <gyandranaupal14@gmail.com>
2026-04-16 22:05:41 +0700

    feat: import all file pages

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,10 +1,21 @@
 import { Route, Routes } from "react-router-dom";
 import HomePages from "../pages/home";
+import MenuPages from "../pages/menu";
+import LokasiPages from "../pages/LokasiPages";
+import OrderHistory from "../pages/orderHistory";
+import CartPages from "../pages/cart";
+import CheckoutPages from "../pages/checkout";
+
 export default function Router() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePages />} />
+        <Route path="/lokasi" element={<LokasiPages />} />
+        <Route path="/menu" element={<MenuPages />} />
+        <Route path="/history-order" element={<OrderHistory />} />
+        <Route path="/cart" element={<CartPages />} />
+        <Route path="/checkout" element={<CheckoutPages />} />
       </Routes>
     </>
   );
