ef9ea88fef67ac5ab495de37076c8427bc732fca
gyandra <gyandranaupal14@gmail.com>
2026-04-19 19:49:13 +0700

    feat: add list product from json

--- src/pages/menu.tsx
+++ src/pages/menu.tsx
@@ -1,7 +1,23 @@
+import ProductCard from "../component/products/productCard";
+import UseProducts from "../hooks/useProducts";
+
 export default function MenuPages() {
+  const { products, isPending, error } = UseProducts();
+
+  if (isPending) {
+    return <p className="text-center ">Loading...</p>;
+  } else if (error) {
+    return <p className="text-center">Terjadi Kesalahan</p>;
+  } else if (!products) {
+    return <p className="text-center">Product Tidak Ditemukan</p>;
+  }
+  console.log(products);
   return (
     <>
-      <div></div>
+      <div>
+        List menu yang kami punya
+        <ProductCard products={products} />
+      </div>
     </>
   );
 }
