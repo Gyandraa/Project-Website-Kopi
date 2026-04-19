e7ed1a37716574777e41298771948e6647a6637e
gyandra <gyandranaupal14@gmail.com>
2026-04-19 19:54:18 +0700

    feat: add conditional if data not found

--- src/service/data.ts
+++ src/service/data.ts
@@ -2,5 +2,7 @@ import { ApiClient } from "./apiClient";
 
 export async function getData() {
   const data = await ApiClient("/products");
-  return data.products;
+
+  if (!data) return [];
+  return data ?? [];
 }
