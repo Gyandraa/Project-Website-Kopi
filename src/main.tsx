32c2f4ec4df8cf0ff00f4060096c4922b35df3a9
gyandra <gyandranaupal14@gmail.com>
2026-04-20 13:36:37 +0700

    just change queryclient from inside in queryclientprovider to in a variable

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,10 @@ import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={new QueryClient()}>
+    <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
   </StrictMode>,
