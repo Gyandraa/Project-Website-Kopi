e77863ba2514e2f64e3b60a3fa4957ae374f67f8
gyandra <gyandranaupal14@gmail.com>
2026-04-20 13:39:36 +0700

    just change validtion from if !data return null to if !data return []

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -1,13 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 import { getData } from "../service/data";
+import type { Products } from "../types/products";
+
 export default function UseProducts() {
-  const { data, isPending, error } = useQuery({
+  const { data, isPending, error } = useQuery<Products[]>({
     queryKey: ["products"],
     queryFn: getData,
+    staleTime: 1000 * 60 * 5,
+    refetchOnWindowFocus: false,
   });
 
   return {
-    products: data || null,
+    products: data ?? [],
     isPending,
     error,
   };
