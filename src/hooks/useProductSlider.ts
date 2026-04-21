f9eaec54fd1251fd9420091d60bae8dd82216d7f
gyandra <gyandranaupal14@gmail.com>
2026-04-21 14:50:09 +0700

    just change name a function from uppercase to lowercase

--- src/hooks/useProductSlider.ts
+++ src/hooks/useProductSlider.ts
@@ -1,11 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import { getData } from "../service/data";
-import type { Products } from "../types/products";
+import type { Products } from "../types/productsTypes";
 
-export default function UseProductSlider() {
+export default function useProductSlider() {
   const { data, isPending, isError, error } = useQuery<Products[]>({
     queryKey: ["productSlider"],
     queryFn: getData,
+    staleTime: 1000 * 60 * 5,
+    refetchOnWindowFocus: false,
   });
 
   return {
