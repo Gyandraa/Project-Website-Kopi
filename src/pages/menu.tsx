cf3ace7f2edda19d6e0ae588577f87b659da8363
gyandra <gyandranaupal14@gmail.com>
2026-04-21 14:44:25 +0700

    style: add styling background color

--- src/pages/menu.tsx
+++ src/pages/menu.tsx
@@ -1,22 +1,27 @@
-import ProductCard from "../component/products/productCard";
-import UseProducts from "../hooks/useProducts";
+import ProductList from "../component/products/productList";
+import useProducts from "../hooks/useProducts";
 
 export default function MenuPages() {
-  const { products, isPending, error } = UseProducts();
+  const { products, isPending, error } = useProducts();
 
   if (isPending) {
     return <p className="text-center ">Loading...</p>;
   } else if (error) {
     return <p className="text-center">Terjadi Kesalahan</p>;
-  } else if (!products) {
+  } else if (products.length === 0) {
     return <p className="text-center">Product Tidak Ditemukan</p>;
   }
-  console.log(products);
+
   return (
     <>
-      <div>
-        List menu yang kami punya
-        <ProductCard products={products} />
+      <div className="py-5 px-4 bg-gray-200">
+        <div className="bg-white rounded-3xl px-10 py-5 ">
+          <h2 className="text-2xl font-bold mb-6 ">
+            Menu <span className="text-amber-500">Kami</span>
+          </h2>
+
+          <ProductList products={products} />
+        </div>
       </div>
     </>
   );
